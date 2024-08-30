b0a0ee9a74f9092d3d8d05fe3cf4b35cbf9c678e
jkga <johnkennethgibasabella@gmail.com>
2024-08-30 08:26:11 +0800

    feat: add a database SQL file wchich includes procedure for getting the next usable ID to be used during an insert operation

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# activity_1
-mySQL/MariaDB insert procedure
+# Activity #1
+This repository contains a MySQL/MariaDB procedure for inserting an item into a database using a newly or recently used primary key.   
+
+## Requirements
+The following conditions must be met for this activity:
+
+- The table must contain columns for **id** and **name**
+- Column **name** must have a corresponding value of **four characters** + the assigned **primary key** of the newly inserted data.
+
+> Sample Format: [abcd1], [xyz2], ... [xyz10]
+
+- The value of the four characters may be ***randomly generated*** or a ***fixed value***
+
+> Sample Fixed Value: [test1], [test2], [test3] ... [test10]
+
+- The value of the inserted **id** should be the smallest unused or removed primary key value.
+
+> ORIGINAL DATA
+
+| ID   | Name  |
+|---   | ---   |
+| 5  |  test5  |
+| 6  |  test6  |
+| 7  |  test7  |
+
+> MODIFIED DATA
+
+| ID   | Name  |
+|---   | ---   |
+| 4  |  test4  |
+| 5  |  test5  |
+| 6  |  test6  |
+| 7  |  test7  |
+
+
+## Tools
+Here is a list of tools and database management systems that could be used for this activity. Please note that you don't have to install MySQL if you already have MariaD.
+- [MariaDB](https://mariadb.org/download/?t=mariadb&p=mariadb&r=11.5.2)
+- [MySQL](https://dev.mysql.com/downloads/)
+- [phpMyAdmin](https://www.phpmyadmin.net/downloads/)
+- [MySQL Workbench](https://dev.mysql.com/downloads/workbench/)
+
+## License
+Copyright (c) 2024 @jkga   
+
+This project is licensed under the terms of the MIT license.
+
---
+++ databases/test.sql
@@ -0,0 +1,133 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Aug 29, 2024 at 05:09 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.3.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test`
+--
+
+CREATE DATABASE IF NOT EXISTS test;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `insert_account`() NO SQL
+BEGIN
+	DECLARE done INT DEFAULT FALSE;
+	DECLARE currentID INT DEFAULT 1;
+  DECLARE nextID INT DEFAULT 1;
+  DECLARE output TEXT DEFAULT '';
+	DECLARE cur CURSOR FOR SELECT id FROM accounts;
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+    
+  OPEN cur;
+    
+  query_loop: LOOP
+    
+      -- current cursor
+    FETCH cur INTO currentID;
+
+      /**
+      * Detect for previously used primary key
+      *
+      * Once the current row has an ID greater than the index counter
+      * it will automatically use its value as the next primary key
+      *
+      *
+      * sample data
+      *|-------------------------|
+      *|       Table             |
+      *|-------------------------|
+      *|  id        |     name   |
+      *|-------------------------|
+      *|  5         |     test5   |
+      *|-------------------------|
+      *|  6         |     test6   |
+      *|-------------------------|
+      *|  7         |     test7   |
+      *|-------------------------|
+      *
+      * currentID: 5
+      * nextID: 1
+      * 
+      */
+      IF currentID > nextID THEN
+        SET output = CONCAT('An old primary_key will be reused: ', nextID);     
+        LEAVE query_loop;
+      END IF;
+      
+      /**
+      * Get the value of the last ID and increment it by 1
+      * this will be used as next primary key if no error has been detected
+      */
+      IF done THEN
+        IF nextID > 1 THEN
+          SET nextID = currentID + 1;
+        END IF;
+        
+        SET output = CONCAT('Data will be appended on the list : ', nextID);  
+        LEAVE query_loop;
+      END IF;
+      
+      /**
+      * Increment cursor every read
+      */
+      SET nextID = nextID + 1;
+
+  END LOOP;
+
+	CLOSE cur;
+
+    SELECT output;
+END$$
+DELIMITER ;
